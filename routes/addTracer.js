c38c7da0bcfc795e05db39effb32699baec1b300
mirajr <mirajr@stanford.edu>
2014-02-18 23:38:12 -0800

    Delete addTracer.js

--- routes/addTracer.js
+++
@@ -1,15 +0,0 @@
-var tracers = require("../tracers.json");
-
-exports.addTracer = function(req, res) {    
-	// Your code goes here
-	var newTracer =  
-		{
-			"name": req.query.name,
-			"description": req.query.description,
-			"imageURL": "http://placekitten.com/300/300"			
-		};
-
-	tracers["tracers"].push(newTracer);
-	console.log(tracers);
-	res.render('addTracer', tracers);
- };
